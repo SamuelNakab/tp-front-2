20015b9b537a1e48de2400c84adc5c1345392944
SamuelNakab <samuelnakab@gmail.com>
2026-05-31 23:05:04 -0300

    A

--- frontend/src/components/react/AppShellBadge.tsx
+++
@@ -1,7 +0,0 @@
-import { useAppShell } from "../../hooks/useAppShell";
-
-export default function AppShellBadge() {
-  const { appName } = useAppShell();
-
-  return <small>React island ready for: {appName}</small>;
-}
